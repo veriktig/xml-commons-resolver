c7230cb58cabb2d04511dde2e33770cb2a862bd8
Fred Gotwald <fred.gotwald@veriktig.com>
2019-07-24 15:59:10 -0700

    Add module-info. 	modified:   .classpath 	new file:   src/module-info.java

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module resolver {
+	exports org.apache.xml.resolver.apps;
+	exports org.apache.xml.resolver.helpers;
+	exports org.apache.xml.resolver;
+	exports org.apache.xml.resolver.readers;
+	exports org.apache.xml.resolver.tools;
+
+	requires java.xml;
+}
